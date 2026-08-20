18d8415445f8cb4fd719700b0e520f3cc02efb19
Edisthon <edisthonshuti14@gmail.com>
2026-08-21 01:56:16 +0200

    Creation of the initial app

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(message="Hello from Flask CI/CD Pipeline!"), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
