eed731c78d9513a80c28ae421d50b10f8c076b8e
Edisthon <edisthonshuti14@gmail.com>
2026-08-28 09:57:09 +0200

    Add new fixes

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -8,6 +8,11 @@ output "prod_server_ip" {
     description = "Public IP of the Production App Server"
 }
 
+output "prod_server_private_ip" {
+    value       = module.prod_server.ec2_instance_private_ip
+    description = "Private IP of the Production App Server (Use this in Jenkins!)"
+}
+
 output "monitoring_server_ip" {
     value       = module.monitoring_server.ec2_instance_ip
     description = "Public IP of the Monitoring Server (Prometheus/Grafana)"
--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -6,8 +6,10 @@ output "ec2_instance_name" {
 }
 
 output "ec2_instance_ip" {
-
     value = aws_instance.jenkins_instance.public_ip
-  
+}
+
+output "ec2_instance_private_ip" {
+    value = aws_instance.jenkins_instance.private_ip
 }
 
